017029ff4803c1e6f0d97741bb9dcafccc5c1f13
Jakub Satora <kubasatora@wp.pl>
2025-03-07 16:47:25 +0100

    bookcover

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import { Button } from './ui/button'
+import BookCover from './BookCover'
 
-const BookOverview = ({ title, author, genre, rating, total_copies, available_copies, description, color, cover }: Book) => {
+const BookOverview = ({ title, author, genre, rating, totalCopies, availableCopies, description, color, cover }: Book) => {
     return (
+
         <section className='book-overview'>
             <div className='flex flex-1 flex-col gap-5'>
                 <h1>
@@ -26,6 +29,41 @@ const BookOverview = ({ title, author, genre, rating, total_copies, available_co
                         <p>
                             {rating}
                         </p>
+                    </div>
+                </div>
+                <div className='book-copies'>
+                    <p>
+                        Total Books: <span>
+
+                            {totalCopies}
+                        </span>
+                    </p>
+                    <p>
+                        Available Books: <span>
+                            {availableCopies}
+                        </span>
+                    </p>
+                </div>
+                <p className='book-description'>
+                    {description}
+                </p>
+                <Button className='book-overvew_btn'>
+                    <Image src="/icons/book.svg" alt="book" width={20} height={20} />
+                    <p className='font-bebas-neue text-xl text-dark-100'>Borrow Book</p>
+                </Button>
+            </div>
+            <div className='relative flex flex-1 justify-center'>
+                <div className='relative'>
+                    <BookCover
+                        variant="wide"
+                        className="z-10"
+                        coverColor={color}
+                        coverImage={cover} />
+                    <div className='absolutr left-16 top-10 rotate-12 opacity-40 max-sm:hidden'>
+                        <BookCover
+                            variant="wide"
+                            coverColor={color}
+                            coverImage={cover} />
                     </div>
                 </div>
             </div>
