7de88543b1af9e32bc86db993a0974d859c85958
Jakub Satora <kubasatora@wp.pl>
2025-03-10 16:22:15 +0100

    variant styles

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-interface Props{
-    calssName?:string;
-    variant:"wide"|"small"
-    coverColor:string;
-    coverUrl:string
+cont variantStyles = {
+  extaraSmall: "book-cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+
+
+}
+interface Props {
+  calssName?: string;
+  variant: "wide" | "small"
+  coverColor: string;
+  coverUrl: string
 }
 
-const BookCover = ({className,variant,coverColor,coverUrl}:Props) => {
+const BookCover = ({ className, variant, coverColor, coverUrl }: Props) => {
   return (
     <div>BookCover</div>
   )
