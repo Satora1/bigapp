2dfff994c7b1334b73cc5781041e13e07043399b
Jakub Satora <kubasatora@wp.pl>
2025-03-26 16:24:32 +0100

    define schema

--- database/schema.ts
+++ database/schema.ts
@@ -1,9 +1,14 @@
-import { pgTable, varchar, uuid } from "drizzle-orm/pg-core";
+import { pgTable, varchar, uuid, text, integer, pgEnum } from "drizzle-orm/pg-core";
+
+export STATUS_ENUM=pgEnum("statsus",["PENDING","APPROVED","REJECTED"])
 
 
-
-
-export const todo = pgTable("todo", {
+export const users = pgTable("users", {
     id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
     fullName: varchar("full_name", { length: 255 }).notNull(),
+    email: text("email").notNull().unique(),
+    universityId: integer("university_id").notNull().unique(),
+    password:text("password").notNull(),
+    universityCard:text("university_card").notNull().unique(),
+    status:""
 })
