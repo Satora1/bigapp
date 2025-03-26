ea6a3140ed69e984900a34713ebf3f1501e4d1e6
Jakub Satora <kubasatora@wp.pl>
2025-03-26 16:26:48 +0100

    some more scema

--- database/schema.ts
+++ database/schema.ts
@@ -1,14 +1,15 @@
 import { pgTable, varchar, uuid, text, integer, pgEnum } from "drizzle-orm/pg-core";
 
-export STATUS_ENUM=pgEnum("statsus",["PENDING","APPROVED","REJECTED"])
-
+export const STATUS_ENUM = pgEnum("statsus", ["PENDING", "APPROVED", "REJECTED"])
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"])
+export const BORROW_STATUS_ENUM = pgEnum("borrow_status", ["BORROWED", "RETURNED"])
 
 export const users = pgTable("users", {
     id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
     fullName: varchar("full_name", { length: 255 }).notNull(),
     email: text("email").notNull().unique(),
     universityId: integer("university_id").notNull().unique(),
-    password:text("password").notNull(),
-    universityCard:text("university_card").notNull().unique(),
-    status:""
+    password: text("password").notNull(),
+    universityCard: text("university_card").notNull().unique(),
+    status: ""
 })
