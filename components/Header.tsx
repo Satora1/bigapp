354a511a6d79001d7730988701e30fb09bb967af
Jakub Satora <kubasatora@wp.pl>
2025-04-07 16:49:00 +0200

    log in/out working , user interface added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { cn } from '@/lib/utils'
+import { cn, getInitials } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
@@ -16,17 +16,21 @@ const Header = ({ session }: { session: Session }) => {
       </Link>
       <ul className='flex flex-row items-center gap-8'>
         <li>
-          <Link href="/library" className={cn("text-base cursor-pointer capitalize", pathName === "/library" ? "text-light-200" : "text-light-100")}>
+          <Link href="/library" className={cn(
+            "text-base cursor-pointer capitalize",
+            pathName === "/library" ? "text-light-200" : "text-light-100")}>
             Library
           </Link>
-          <li>
-            <Link href="/my-profile">
-              <Avatar>
+        </li>
+        <li>
+          <Link href="/my-profile">
+            <Avatar>
 
-                <AvatarFallback>{session?.user?.name}</AvatarFallback>
-              </Avatar>
-            </Link>
-          </li>
+              <AvatarFallback className='bg-amber-100'>
+                {getInitials(session?.user?.name || "IN")}</AvatarFallback>
+            </Avatar>
+          </Link>
+
         </li>
       </ul>
     </header>
