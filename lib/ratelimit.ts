3c87d4142cca61fe24f46484b21886b2cc830596
Jakub Satora <kubasatora@wp.pl>
2025-04-08 16:26:18 +0200

    next workflow

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -3,7 +3,7 @@ import { Ratelimit } from "@upstash/ratelimit";
 
 const rateLimit = new Ratelimit({
     redis,
-    limiter: Ratelimit.fixedWindow(5, "1 m"),
+    limiter: Ratelimit.fixedWindow(3, "1 m"),
     analytics: true,
     prefix: "@upstash/ratelimit",
 })
