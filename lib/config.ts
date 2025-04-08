4791ae41b2d4217ac8df9646d7e4ef888ad800ea
Jakub Satora <kubasatora@wp.pl>
2025-04-08 15:42:38 +0200

    rate limit

--- lib/config.ts
+++ lib/config.ts
@@ -1,14 +1,19 @@
 import { url } from "inspector";
 
-const config ={
-    env:{
-        apiEndpoint:process.env.NEXT_PUBLIC_API_ENDPOINT!,
-        imagekit:{
-            publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-            urlEndpoint:process.env.NEXT_PUBLIC_IMAGE_URL_ENDPOINT!,
-            privatKey:process.env.IMAGEKIT_PRIVATE_KEY!
+const config = {
+    env: {
+        apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+        imagekit: {
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGE_URL_ENDPOINT!,
+            privatKey: process.env.IMAGEKIT_PRIVATE_KEY!
         },
-        databaseURL:process.env.DATABASE_URL!,
+        databaseURL: process.env.DATABASE_URL!,
+        upstash: {
+            rdisUrl: process.env.UPSTASH_REDIS_URL,
+            redisToken: process.env.UPSTASH_REDIS_TOKEN,
+        },
+
     }
 }
 
