859fa0579164afdf51be76089ac4da92b10e27cc
Jakub Satora <kubasatora@wp.pl>
2025-04-11 16:17:36 +0200

    refactor mutations

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -6,3 +6,34 @@ export const workflowClient = new WorkflowClient({
     baseUrl: config.env.upstash.qstashUrl,
     token: config.env.upstash.qstashToken,
 })
+
+
+const qstashClient = new QStashClient({
+    token: config.env.upstash.qstashToken
+});
+
+export const sendEmail = async ({
+    emial, subject, message
+}: {
+    emial: string,
+    subject: string,
+    message: string
+
+}) => {
+    await qstashClient.publishJSON({
+        api: {
+            name: "email",
+            provider: resend({ token: config.env.resendToken }),
+        },
+        body: {
+            from: "JS.shop <hello.jakubsatroa.shop>",
+            to: [emial],
+            subject,
+            html: message,
+        },
+    });
+}
+
+
+
+
