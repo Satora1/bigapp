03a4768f423109096737f3e220800f1411f08c10
Jakub Satora <kubasatora@wp.pl>
2025-04-14 15:54:21 +0200

    try this

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -3,5 +3,5 @@ import { neon } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-http"
 
 
-const sql = neon(config.env.databaseURL)
+const sql = neon(config.env.databaseUrl)
 export const db = drizzle({ client: sql })
