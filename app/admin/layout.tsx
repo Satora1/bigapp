ee5f2831e9a93ed7a0c1bfd16dfd4cf976886d8a
Jakub Satora <kubasatora@wp.pl>
2025-04-16 15:35:51 +0200

    more admin stuff

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,15 @@
 import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 import React, { ReactNode } from 'react'
-
+import "@/styles/admin.css"
+import Sidebar from '@/components/admin/Sidebar'
 const layout = async ({ children }: { children: ReactNode }) => {
     const session = await auth()
     if (!session?.user?.id) redirect("/sign-in")
     return (
         <main className='flex min-h-screen flex-row'>
             <p>
-                Sidebar
+                <Sidebar />
             </p>
             <div className='admin-container'>
                 <p>
---
+++ components/admin/Sidebar.tsx
@@ -0,0 +1,40 @@
+import { adminSideBarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='admin-sidebar'>
+      <div>
+        <div className='logo'>
+          <Image src="/icons/admin/logo.svg" alt="logo" height={37} width={37} />
+          <h1>
+            BookWise
+          </h1>
+        </div>
+        <div className='mt-10 flex flex-col gap-5'>
+          {adminSideBarLinks.map((link) => {
+            const isSelected = false;
+
+            return (
+              <Link href={link.route} key={link.route}>
+                <div className={cn("link", isSelected && "bg-primary-admin shadow-sm")}>
+                  <div className='relative size-5'>
+                    <Image src={link.img} alt="icon" fill className={`${isSelected}? "brightness-0 invert" : '' object-containe`} />
+                  </div>
+                </div>
+              </Link>
+
+
+            )
+          })}
+
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default Sidebar
