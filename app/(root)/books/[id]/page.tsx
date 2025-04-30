62f6dd4a34f4d50f3bd0f36eca768854274d8d61
Jakub Satora <kubasatora@wp.pl>
2025-04-30 10:43:44 +0200

    book summary

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -19,12 +19,31 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   if (!bookDetails) redirect("/404");
 
-    return (
-        <>
-   <BookOverview {...bookDetails} userId={session?.user?.id as string} />
-
-        </>
-    )
+  return (
+    <>
+      <BookOverview {...bookDetails} userId={session?.user?.id as string} />
+      <div className="book-details">
+        <div className="flex-[1.5]">
+          <section className="flex flex-col gap-7">
+            <h3>
+              Video
+            </h3>
+            Video Component
+          </section>
+          <section className="mt-10 flex flex-col gap-7">
+            <h3>
+              Summary
+            </h3>
+            <div className="space-y-5 text-xl text-light-100">
+              {bookDetails.summary.split("\n").map((line, i) => (
+                <p key={i}>{line}</p>
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Page
