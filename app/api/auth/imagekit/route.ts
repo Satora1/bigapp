53ca770870262d47bef6cdf04fb9096c627b9284
Jakub Satora <kubasatora@wp.pl>
2025-04-30 15:02:57 +0200

    cors fixing test

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -5,12 +5,32 @@ import { NextResponse } from "next/server";
 const { env: { imagekit: { publicKey, privateKey, urlEndpoint } } } = config;
 
 const imagekit = new ImageKit({
-    publicKey: publicKey,
-    privateKey: privateKey,
-    urlEndpoint: urlEndpoint
-})
-
+  publicKey,
+  privateKey,
+  urlEndpoint
+});
 
 export async function GET() {
-    return NextResponse.json(imagekit.getAuthenticationParameters())
+  const authParams = imagekit.getAuthenticationParameters();
+
+  return new NextResponse(JSON.stringify(authParams), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "https://bigapp-n6wpmnuut-satora1s-projects.vercel.app",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
+}
+
+// obsługa preflight OPTIONS (ważne!)
+export function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "https://bigapp-n6wpmnuut-satora1s-projects.vercel.app",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
 }
