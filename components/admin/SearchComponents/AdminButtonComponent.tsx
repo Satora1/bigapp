ac5a6ecccb1d02cef65061c00f6916c1c8366337
Jakub Satora <kubasatora@wp.pl>
2025-06-10 14:48:29 +0200

    some refactor

--- components/admin/SearchComponents/AdminButtonComponent.tsx
+++ components/admin/SearchComponents/AdminButtonComponent.tsx
@@ -1,15 +1,19 @@
+
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { Button } from "../../ui/button";
 import { auth } from "@/auth";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+
 
 const AdminButton = async () => {
     const session = await auth();
     const email = session?.user?.email;
 
     if (!email) return null;
+
 
     const [user] = await db
         .select()
@@ -21,10 +25,14 @@ const AdminButton = async () => {
 
     return (
         <form>
-            <Button className="h-20 w-40">
+            <Button className="h-20 w-40"  >
+                <Link href="/admin">
+                    <h2 className="text-xl bold"
+                    >
 
-                <Link href="/admin"><h2 className="text-xl bold">
-                    Admin </h2></Link>
+                        Admin </h2>
+                </Link>
+
             </Button>
         </form>
     );
